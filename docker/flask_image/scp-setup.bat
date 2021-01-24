5581b0850cee34f1e1ac1da8e39d26b03e671ea1
kayaru28 <istorytale090415@gmail.com>
2021-01-24 17:02:03 +0900

    recursive

--- docker/flask_image/scp-setup.bat
+++ docker/flask_image/scp-setup.bat
@@ -1,4 +1,4 @@
-scp  D:\WORK\b418_STUDY_git\training_set\docker\flask_image\setup\* root@10.255.255.1:/root/dockerfiles/001_python_tool/setup/
+scp -r D:\WORK\b418_STUDY_git\training_set\docker\flask_image\setup\* root@10.255.255.1:/root/dockerfiles/001_python_tool/setup/
 
 
 
