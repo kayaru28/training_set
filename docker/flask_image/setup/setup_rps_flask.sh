fb3660624657717979bd98084c968ae6dd156bd2
kayaru28 <istorytale090415@gmail.com>
2021-01-25 23:16:55 +0900

    add : for rm

--- docker/flask_image/setup/setup_rps_flask.sh
+++ docker/flask_image/setup/setup_rps_flask.sh
@@ -2,6 +2,10 @@
 source /root/setup/setup_rps_common.sh
 export root_password=$(cat /root/setup/secret.txt |grep root_pass| awk '{print $1}')
 
+rm -f app-python.zip*
+rm -f *.py
+rm -r -f templates
+
 download_zip="app-python.zip"
 wget https://raw.githubusercontent.com/kayaru28/training_set/master/docker/flask_image/${download_zip}
 unzip ${download_zip}
