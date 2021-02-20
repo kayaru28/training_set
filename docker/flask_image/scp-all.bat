f5254d639cc68fbb12fb4d8f99930cdd1648dbe0
kayaru28 <istorytale090415@gmail.com>
2021-02-20 19:41:15 +0900

    update : comannds

--- docker/flask_image/scp-all.bat
+++ docker/flask_image/scp-all.bat
@@ -1,3 +1,20 @@
-scp-dockerfile.bat
-scp-setup.bat
-scp-yml.bat
+scp -i C:\Users\istor\.ssh\id_rsa_pc4 D:\WORK\b418_STUDY_git\training_set\docker\flask_image\dockerfile_*.df root@10.255.255.1:/root/dockerfiles/001_python_tool/
+scp -i C:\Users\istor\.ssh\id_rsa_pc4 -r D:\WORK\b418_STUDY_git\training_set\docker\flask_image\fluentd\* root@10.255.255.1:/root/dockerfiles/001_python_tool/fluentd/
+scp -i C:\Users\istor\.ssh\id_rsa_pc4 -r D:\WORK\b418_STUDY_git\training_set\docker\flask_image\setup\* root@10.255.255.1:/root/dockerfiles/001_python_tool/setup/
+scp -i C:\Users\istor\.ssh\id_rsa_pc4 -r D:\WORK\b418_STUDY_git\training_set\docker\flask_image\test\* root@10.255.255.1:/root/dockerfiles/001_python_tool/test/
+scp -i C:\Users\istor\.ssh\id_rsa_pc4 D:\WORK\b418_STUDY_git\training_set\docker\flask_image\*.yml root@10.255.255.1:/root/dockerfiles/001_python_tool/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
