1818c323a7e026d83684a59a156d7ed57fa10607
kayaru28 <istorytale090415@gmail.com>
2021-06-11 11:41:10 +0900

    add : redminegit statusgit status!

---
+++ docker/redmine/scp-yml.bat
@@ -0,0 +1,6 @@
+set destination_host=root@192.168.1.240
+set source_file=D:\WORK\b418_STUDY_git\training_set\docker\redmine\*.yml
+set destination_directory=/root/dockerfiles/002_redmine/
+
+scp -i C:\Users\istor\.ssh\id_rsa_pc4 -r %source_file% %destination_host%:%destination_directory%
+
