9fe483cbab913b782d95d6a236ffa55e5b9a3248
kayaru28 <istorytale090415@gmail.com>
2021-07-17 20:04:59 +0900

    update : mpdify ssh key name

--- docker/001_flask_image/scp-all.bat
+++ docker/001_flask_image/scp-all.bat
@@ -2,27 +2,27 @@ set destination_host=root@192.168.1.240
 set source_file=D:\WORK\b418_STUDY_git\training_set\docker\001_flask_image\dockerfile_*.df
 set destination_directory=/root/dockerfiles/001_python_tool/
 
-scp -i C:\Users\istor\.ssh\id_rsa_grun -r %source_file% %destination_host%:%destination_directory%
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
 
 set source_file=D:\WORK\b418_STUDY_git\training_set\docker\001_flask_image\fluentd\*
 set destination_directory=/root/dockerfiles/001_python_tool/fluentd/
 
-scp -i C:\Users\istor\.ssh\id_rsa_grun -r %source_file% %destination_host%:%destination_directory%
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
 
 set source_file=D:\WORK\b418_STUDY_git\training_set\docker\001_flask_image\setup\*
 set destination_directory=/root/dockerfiles/001_python_tool/setup/
 
-scp -i C:\Users\istor\.ssh\id_rsa_grun -r %source_file% %destination_host%:%destination_directory%
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
 
 set source_file=D:\WORK\b418_STUDY_git\training_set\docker\001_flask_image\test\*
 set destination_directory=/root/dockerfiles/001_python_tool/test/
 
-scp -i C:\Users\istor\.ssh\id_rsa_grun -r %source_file% %destination_host%:%destination_directory%
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
 
 set source_file=D:\WORK\b418_STUDY_git\training_set\docker\001_flask_image\*.yml
 set destination_directory=/root/dockerfiles/001_python_tool/
 
-scp -i C:\Users\istor\.ssh\id_rsa_grun -r %source_file% %destination_host%:%destination_directory%
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
 
 
 
--- docker/001_flask_image/scp-test.bat
+++ docker/001_flask_image/scp-test.bat
@@ -3,7 +3,7 @@ set destination_host=root@192.168.1.240
 set source_file=D:\WORK\b418_STUDY_git\training_set\docker\001_flask_image\test\*
 set destination_directory=/root/dockerfiles/001_python_tool/test/
 
-scp -i C:\Users\istor\.ssh\id_rsa_grun -r %source_file% %destination_host%:%destination_directory%
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
 
 
 
