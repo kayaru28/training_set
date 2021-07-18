33989c405cbb69eb73ac6eabf122658e95d53ae2
kayaru28 <istorytale090415@gmail.com>
2021-07-19 00:12:18 +0900

    add : ansible set

---
+++ docker/003_ansible/scp-ansible.bat
@@ -0,0 +1,6 @@
+set destination_host=root@192.168.1.240
+set source_file=D:\WORK\b418_STUDY_git\training_set\docker\003_ansible\*
+set destination_directory=/root/dockerfiles/003_ansible/
+
+scp -i C:\Users\istor\.ssh\id_rsa_verdigris -r %source_file% %destination_host%:%destination_directory%
+
